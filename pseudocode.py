64207f1e3009b12f2b7f4f6076264c0c80b7812c
c000927 <54864199+c000927@users.noreply.github.com>
2019-10-11 13:40:57 -0600

    Update pseudocode.py
    Fix some bugs

--- pseudocode.py
+++ pseudocode.py
@@ -1,19 +1,18 @@
 start with a soldier on a helicopter with choices of weapon:
-Assult: M4A1 35 Attack Points, with 20% chance to miss
-CQB: HK MP5 30 Attack Points, with 30% chance to miss
-Demolition: M870 80 Attack Points, with 50% chance to miss
-Supporter: M249 SAW 70 Attack Points, with 35% chance to miss
-Sniper: MK 20 Mod 0 SSR 100 Attack Points, kill mmediantly, with nothing else
+Assult: M4A1 35 Attack Points, with 20% chance to miss, 100 Health Points
+CQB: HK MP5 30 Attack Points, with 30% chance to miss, 110 Health Points
+Demolition: M870 80 Attack Points, with 50% chance to miss, 110 Health Points
+Supporter: M249 SAW 70 Attack Points, with 35% chance to miss, 70 Health Points
+Sniper: MK 20 Mod 0 SSR 100 Attack Points, 15% chance to miss, 40 Health Points
 
 After select weapon, soldier will drop into the war zone, on a Light Strike Vehicle
-LSV has 50 armor Points
 
 Enters enemy control zone
 Random generate enemy from 2 sentinels to 4 sentinels
 
 decide if to kill or avoid
 if kill:
-    if kill in 5 rounds:
+    if kill in 10 rounds:
         sucess and proceed
     else:
         fail and be discovered
@@ -23,11 +22,11 @@ if avoid:
     70% of sucess and proceed
 
 pick random medication and apply to their hp
-Bandage + 10hp
-Energy Drink + 15hp
-First Aid Kit + 25hp
-Painkillers + 35hp
-Med Kit + 50hp
+Bandage + 10hp 30% to drop
+Energy Drink + 15hp 30% to drop
+First Aid Kit + 25hp 20% to drop
+Painkillers + 35hp 10% to drop
+Med Kit + 50hp 10% to drop
 
 next step: blow up the enemy generator
 Random generate enemy from 4 sentinels or 6 sentinels
@@ -53,7 +52,7 @@ if yes:
     generate 1 enemy tank and 20 enemies
     decide if to call air Support
     if yes:
-        all enemy blown up
+        all enemy blown up and add 10 enemy support soldiers
     if no:
         fight
 if no:
@@ -73,3 +72,8 @@ if live:
     you win
 else:
     you lost
+
+Future Plan:
+Add inventory along the game
+Add aim system
+Add more story system
